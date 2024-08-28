6256fa7e46a7aaa799241b91f00220095f5efbda
Ravi Kumar <ravi.gupta.plc.22@gmail.com>
2024-08-29 00:22:42 +0530

    added the docker concept

--- backend/src/index.js
+++ backend/src/index.js
@@ -25,6 +25,18 @@ const exitSchema = z.object({
 app.use(express.json());
 
 //main route
+app.get("/get-employee-details", async (req, res) => {
+    try {
+        const response = await sheets.spreadsheets.values.get({
+            spreadsheetId: process.env.SHEET_ID,
+            range: "Empolyee-Information!A2:G"
+        })
+        res.status(200).json({ data: response.data.values })
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+})
+
 app.post("/mark-entry", async (req, res) => {
     try {
         const body = entrySchema.parse(req.body);
