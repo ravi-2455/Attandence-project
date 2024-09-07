6bdab1942426a38e424e28727cb431b486d16edb
Ravi Kumar <ravi.gupta.plc.22@gmail.com>
2024-09-07 09:28:13 +0530

    improved tuition status api

--- backend/src/controllers/adminControllers.js
+++ backend/src/controllers/adminControllers.js
@@ -12,20 +12,53 @@ const tuitionStatusSchema = z.object({
   reason: z.string().min(1, { message: "reason is required" }),
 });
 
+const markEmployeeStatusClose = async (date, ele) => {
+  //mark this employee status close
+  const eid = ele[0];
+  ele[0] = date;
+  await sheets.spreadsheets.values.append({
+    spreadsheetId: process.env.SHEET_ID,
+    range: `${eid}!A3:C`,
+    insertDataOption: "INSERT_ROWS",
+    valueInputOption: "RAW",
+    requestBody: {
+      values: [ele],
+    },
+  });
+};
+
+const markAllEmployeeStatusClose = async (date) => {
+  //eid,name,status as closed
+  //get eid of all the employee
+  const response = await sheets.spreadsheets.values.get({
+    spreadsheetId: process.env.SHEET_ID,
+    range: "Empolyee-Information!A2:B",
+  });
+
+  const sheetData = response.data.values;
+
+  sheetData.map((ele) => {
+    ele.push("close");
+    markEmployeeStatusClose(date, ele);
+  });
+};
+
 export const addTuitionStatus = async (req, res) => {
   try {
     const body = tuitionStatusSchema.parse(req.body);
-    const row = Object.values(body);
+    const { date, status, reason } = body;
+    if (status === "close") {
+      markAllEmployeeStatusClose(date);
+    }
 
+    const row = Object.values(body);
     //geting the data
     const response = await sheets.spreadsheets.values.get({
       spreadsheetId: process.env.SHEET_ID,
       range: "tuition-status!A2:C",
     });
-
     //data from get request
     const sheetData = response.data.values;
-
     //checking for empty condition in data recieved from the get call
     if (sheetData) {
       if (row[0] === sheetData[sheetData.length - 1][0]) {
